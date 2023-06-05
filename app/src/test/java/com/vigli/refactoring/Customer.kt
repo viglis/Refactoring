10e62ed21da3af2bcf72ec382087978e18d17a91
vigli <vigli@kmong.com>
2023-06-06 01:17:42 +0900

    임시변수 제거

--- app/src/test/java/com/vigli/refactoring/Customer.kt
+++ app/src/test/java/com/vigli/refactoring/Customer.kt
@@ -9,20 +9,24 @@ data class Customer(
     }
 
     fun statement(): String {
-        var frequentRenterPoints = 0
         var result = "$name 고객님의 대여 기록\n"
 
         rentals.forEach { rental ->
-            // 경우에 따른 적립 포인트 지급 함수를 호출
-            frequentRenterPoints += rental.getFrequentRenterPoints()
-
             // 이번에 대여하는 비디오 정보와 대여료를 출력
             result += "\t${rental.movie.title}\t${rental.getCharge()}\n"
         }
 
         // 푸터 행 추가
         result += "누적 대여료: ${getTotalCharge()}\n"
-        result += "적립 포인트: $frequentRenterPoints"
+        result += "적립 포인트: ${getTotalFrequentRenterPoints()}"
+        return result
+    }
+
+    private fun getTotalFrequentRenterPoints(): Int {
+        var result = 0
+        rentals.forEach { rental ->
+            result += rental.getFrequentRenterPoints()
+        }
         return result
     }
 
