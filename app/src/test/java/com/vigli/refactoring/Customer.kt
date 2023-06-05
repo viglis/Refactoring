7b94dca0296f70ae29d9b0d781dbb7d22214a390
vigli <vigli@kmong.com>
2023-06-06 01:20:33 +0900

    html 형식으로 만들어주는 메소드도 내부 동작은 statement 에서 호출하는 메소드를 재사용을 한다.

--- app/src/test/java/com/vigli/refactoring/Customer.kt
+++ app/src/test/java/com/vigli/refactoring/Customer.kt
@@ -22,6 +22,20 @@ data class Customer(
         return result
     }
 
+    fun htmlStatement(): String {
+        var result = "<h1><em>$name</em> 고객님의 대여 기록</h1><p>\n"
+
+        rentals.forEach { rental ->
+            // 이번에 대여하는 비디오 정보와 대여료를 출력
+            result += "${rental.movie.title}: ${rental.getCharge()}<br>\n"
+        }
+
+        // 푸터 행 추가
+        result += "<p>누적 대여료: <em>${getTotalCharge()}</em><p>\n"
+        result += "적립 포인트: <em>${getTotalFrequentRenterPoints()}</em><p>"
+        return result
+    }
+
     private fun getTotalFrequentRenterPoints(): Int {
         var result = 0
         rentals.forEach { rental ->
