05c1e697a9069d23bb111695a800328f38de6381
vigli <vigli@kmong.com>
2023-06-08 21:43:03 +0900

    가격 책정 조건문 재정의로 교체
    비디오 종류가 변경해도 미치는 영향을 최소화 한다.

--- app/src/test/java/com/vigli/refactoring/Movie.kt
+++ app/src/test/java/com/vigli/refactoring/Movie.kt
@@ -1,6 +1,37 @@
 package com.vigli.refactoring
 
 data class Movie(val title: String, var priceCode: Int) {
+    fun getCharge(daysRented: Int): Double {
+        var result = 0.0
+        when (priceCode) {
+            Movie.REGULAR -> {
+                result += 2
+                if (daysRented > 2) {
+                    result += (daysRented - 2) * 1.5
+                }
+            }
+            Movie.NEW_RELEASE -> {
+                result += daysRented * 3
+            }
+            Movie.CHILDRENS -> {
+                result += 1.5
+                if (daysRented > 3) {
+                    result += (daysRented - 3) * 1.5
+                }
+            }
+        }
+        return result
+    }
+
+    fun getFrequentRenterPoints(daysRented: Int): Int {
+        // 최신물을 이틀 이상 대여하면 보너스 포인트 지급
+        if (priceCode == Movie.NEW_RELEASE &&
+            daysRented > 1
+        ) {
+            return 2
+        }
+        return 1
+    }
 
     companion object {
         const val CHILDRENS = 2
--- app/src/test/java/com/vigli/refactoring/Rental.kt
+++ app/src/test/java/com/vigli/refactoring/Rental.kt
@@ -5,34 +5,10 @@ data class Rental(
     val daysRented: Int
 ) {
     fun getCharge(): Double {
-        var result = 0.0
-        when (movie.priceCode) {
-            Movie.REGULAR -> {
-                result += 2
-                if (daysRented > 2) {
-                    result += (daysRented - 2) * 1.5
-                }
-            }
-            Movie.NEW_RELEASE -> {
-                result += daysRented * 3
-            }
-            Movie.CHILDRENS -> {
-                result += 1.5
-                if (daysRented > 3) {
-                    result += (daysRented - 3) * 1.5
-                }
-            }
-        }
-        return result
+        return movie.getCharge(daysRented)
     }
 
     fun getFrequentRenterPoints(): Int {
-        // 최신물을 이틀 이상 대여하면 보너스 포인트 지급
-        if (movie.priceCode == Movie.NEW_RELEASE &&
-            daysRented > 1
-        ) {
-            return 2
-        }
-        return 1
+        return movie.getFrequentRenterPoints(daysRented)
     }
 }
