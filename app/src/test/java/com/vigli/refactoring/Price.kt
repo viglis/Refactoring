bef0def087a13135853da419308f0b2669a5680a
vigli <vigli@kmong.com>
2023-06-08 22:10:35 +0900

    가격 책정 조건문 재정의로 변경

--- app/src/test/java/com/vigli/refactoring/Price.kt
+++ app/src/test/java/com/vigli/refactoring/Price.kt
@@ -3,32 +3,20 @@ package com.vigli.refactoring
 abstract class Price {
     abstract fun getPriceCode(): Int
 
-    fun getCharge(daysRented: Int): Double {
-        var result = 0.0
-        when (getPriceCode()) {
-            Movie.REGULAR -> {
-                result += 2
-                if (daysRented > 2) {
-                    result += (daysRented - 2) * 1.5
-                }
-            }
-            Movie.NEW_RELEASE -> {
-                result += daysRented * 3
-            }
-            Movie.CHILDRENS -> {
-                result += 1.5
-                if (daysRented > 3) {
-                    result += (daysRented - 3) * 1.5
-                }
-            }
-        }
-        return result
-    }
+    abstract fun getCharge(daysRented: Int): Double
 }
 
 class ChildrensPrice : Price() {
     override fun getPriceCode(): Int {
         return Movie.CHILDRENS
+    }
+
+    override fun getCharge(daysRented: Int): Double {
+        var result = 1.5
+        if (daysRented > 3) {
+            result += (daysRented - 3) * 1.5
+        }
+        return result
     }
 }
 
@@ -36,10 +24,22 @@ class NewReleasePrice : Price() {
     override fun getPriceCode(): Int {
         return Movie.NEW_RELEASE
     }
+
+    override fun getCharge(daysRented: Int): Double {
+        return daysRented * 3.0
+    }
 }
 
 class RegularPrice : Price() {
     override fun getPriceCode(): Int {
         return Movie.REGULAR
+    }
+
+    override fun getCharge(daysRented: Int): Double {
+        var result = 2.0
+        if (daysRented > 2) {
+            result += (daysRented - 2) * 1.5
+        }
+        return result
     }
 }
